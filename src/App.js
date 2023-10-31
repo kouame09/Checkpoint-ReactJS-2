e41389cd4f3189d3c8c73fe8329be30843a654ae
kouame09 <princekouame7@gmail.com>
2023-10-31 17:13:50 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./navBar.js"; 
+import Cards from "./cards.js"; 
+
+function App() {
+  return (
+    <div>
+      <NavBar /> 
+      <Cards /> 
+      <Cards /> 
+      <Cards /> 
+    </div>
+  );
+}
+
+export default App;
+
+
